d9040125a1adf1c0ea72f031e4cf46255aafa4ab
reemias <141964407+reemias@users.noreply.github.com>
2024-02-01 16:50:20 -0300

    confirmar alterações

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 //faça  aparecer uma mensagem de alerta 
-alert("bora jogar !");
-// crie uma variavel para armazenar o numero do jogo
-let numeroSecreto = 29;
-// crie outra variavel para colocar o numero digitado no prompt
-let chute = prompt('escolha um número de 1 a 100');
-// se o chute for igual ao numerosecreto 
-if (chute == numeroSecreto) {
-    // então execute a menssagem no console.log
-    console.log ('você acertou o número secreto (29)');
 
-} else alert ('vc errou o número');
+alert("bora jogar !");
+
+// crie uma variavel para armazenar o numero do jogo
+
+let numeroSecreto = 29;
+
+// mostre o numero secreto no console.log
+
+console.log (numeroSecreto)
+
+// crie outra variavel para colocar o numero digitado no prompt
+
+let chute = prompt('escolha um número de 1 a 100');
+
+// se o chute for igual ao numerosecreto 
+
+if (chute == numeroSecreto) {
+
+    // então execute a menssagem 
+    alert (`você acertou o número secreto ${numeroSecreto}`);
+
+} // se não mostre essa menssagem
+else alert ('vc errou o número');
